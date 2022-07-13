9f00d1bc4519c552057bced4b8c950ece5330a5b
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-13 10:59:12 +0430

    Create the test case to check that deposit to an account that its balance is more than 0 should do correctly.

--- src/test/kotlin/com/banking/AccountTest.kt
+++ src/test/kotlin/com/banking/AccountTest.kt
@@ -1,14 +1,29 @@
 package com.banking
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AccountTest {
+
+    private lateinit var account: Account
+
+    @BeforeAll
+    fun setup() {
+        account = Account()
+    }
 
     @Test
     fun `should deposit into an account correctly`() {
-        val accountTest = Account()
-        accountTest.deposit(1000)
-        assertEquals(1000, accountTest.getBalance())
+        account.deposit(1000)
+        assertEquals(1000, account.getBalance())
+    }
+
+    @Test
+    fun `should deposit into an account that its balance is more than 0 correctly`() {
+        account.deposit(500)
+        assertEquals(1500, account.getBalance())
     }
 }
