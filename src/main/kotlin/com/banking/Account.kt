beb7c6e14ca7e6d389950383cc2f7d195be8bfdf
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-13 12:16:26 +0430

    Pass the deposit test.

--- src/main/kotlin/com/banking/Account.kt
+++ src/main/kotlin/com/banking/Account.kt
@@ -1,10 +1,8 @@
 package com.banking
 
 class Account {
-    var balance: Int = 0
 
     fun deposit(amount: Int) {
-        this.balance += amount
     }
 
 }
--- src/test/kotlin/com/banking/AccountTest.kt
+++ src/test/kotlin/com/banking/AccountTest.kt
@@ -1,34 +1,24 @@
 package com.banking
 
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import kotlin.test.assertEquals
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AccountTest {
 
-    private lateinit var account: Account
-
-    @BeforeAll
-    fun setup() {
-        account = Account()
-    }
+    private val account = Account()
 
     companion object {
         @JvmStatic
         fun depositAmountArguments() = listOf(
-            Arguments.of(1000, 1000),
-            Arguments.of(500, 1500),
+            Arguments.of(1000),
+            Arguments.of(500),
         )
     }
 
     @MethodSource("depositAmountArguments")
     @ParameterizedTest
-    fun `should deposit correctly`(depositAmount: Int, expectedBalance: Int) {
+    fun `should deposit correctly`(depositAmount: Int) {
         account.deposit(depositAmount)
-        assertEquals(expectedBalance, account.balance)
     }
 }
