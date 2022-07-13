8cf88fd95614208411b96c2e2e8fdc70f2257af7
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-13 15:07:04 +0430

    Implement deposit, withdraw and printing header of account statement.

--- src/main/kotlin/com/banking/Account.kt
+++ src/main/kotlin/com/banking/Account.kt
@@ -1,12 +1,28 @@
 package com.banking
 
-class Account {
+class Account(private val printer: (input: String) -> Unit) {
+    private var balance = 0
 
     fun deposit(amount: Int) {
+        balance += amount
     }
 
     fun withdraw(amount: Int) {
-        throw InsufficientBalanceException()
+        if (balance < amount)
+            throw InsufficientBalanceException()
+        balance -= amount
     }
 
+    fun printStatement() {
+        printer("DATE       | AMOUNT  | BALANCE")
+    }
+
+}
+
+
+fun main() {
+    val printer: (input: String) -> Unit = { println(it) }
+    val account = Account(printer)
+
+    account.printStatement()
 }
--- src/test/kotlin/com/banking/AccountTest.kt
+++ src/test/kotlin/com/banking/AccountTest.kt
@@ -1,5 +1,6 @@
 package com.banking
 
+import io.mockk.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -8,24 +9,70 @@ import org.junit.jupiter.params.provider.MethodSource
 
 class AccountTest {
 
-    private val account = Account()
+    private val account = Account(mockk())
 
     companion object {
         @JvmStatic
-        fun depositAmountArguments() = listOf(
+        fun amountArguments() = listOf(
             Arguments.of(1000),
             Arguments.of(500),
+            Arguments.of(200),
+            Arguments.of(300),
+        )
+
+        @JvmStatic
+        fun depositAndWithdrawArguments() = listOf(
+            Arguments.of(listOf(1000), listOf(500)),
+            Arguments.of(listOf(1000, 1000), listOf(1500)),
+        )
+
+        @JvmStatic
+        fun depositAndWithdrawInvalidArguments() = listOf(
+            Arguments.of(listOf(1000, 1000), listOf(1500), 700),
+            Arguments.of(listOf(1000, 1000), listOf(1500), 800),
         )
     }
 
-    @MethodSource("depositAmountArguments")
+    @MethodSource("amountArguments")
     @ParameterizedTest
     fun `should deposit correctly`(depositAmount: Int) {
         account.deposit(depositAmount)
     }
 
+    @MethodSource("amountArguments")
+    @ParameterizedTest
+    fun `should raise exception when withdrawing from empty account`(withdrawAmount: Int) {
+        assertThrows<InsufficientBalanceException> { account.withdraw(withdrawAmount) }
+    }
+
+    @ParameterizedTest
+    @MethodSource("depositAndWithdrawArguments")
+    fun `should be able to withdraw after deposit`(depositList: List<Int>, withdrawList: List<Int>) {
+        depositList.forEach { account.deposit(it) }
+        withdrawList.forEach { account.withdraw(it) }
+    }
+
+    @ParameterizedTest
+    @MethodSource("depositAndWithdrawInvalidArguments")
+    fun `should raise exception when withdrawing more than balance`(
+        depositList: List<Int>,
+        withdrawList: List<Int>,
+        extraWithdrawAmount: Int
+    ) {
+        `should be able to withdraw after deposit`(depositList, withdrawList)
+        assertThrows<InsufficientBalanceException> {
+            account.withdraw(extraWithdrawAmount)
+        }
+    }
+
     @Test
-    fun `should raise exception when withdrawing from empty account`(){
-        assertThrows<InsufficientBalanceException> { account.withdraw(200) }
+    fun `should print account statement`() {
+        val printer = mockk<(input: String) -> Unit>()
+        every { printer("DATE       | AMOUNT  | BALANCE") } just runs
+
+        val account = Account(printer)
+        account.printStatement()
+
+        verify { printer("DATE       | AMOUNT  | BALANCE") }
     }
 }
